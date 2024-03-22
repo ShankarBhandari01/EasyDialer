88c1085f06bd649915ab7c26035e257c342bea9a
Shankar Bhandari <harryshankar@outlook.com>
2024-03-22 22:18:00 +0545

    adds validation in forget password activity

--- app/src/main/java/com/example/easydialer/ui/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/easydialer/ui/ForgotPasswordActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.example.easydialer.databinding.ActivityForgotPasswordBinding
+import com.example.easydialer.utils.SweetToast
 import com.example.easydialer.viewmodels.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,6 +22,25 @@ class ForgotPasswordActivity : AppCompatActivity() {
             var newPassword = newpassword.text.toString()
             var confirmPassword = confirmpassword.text.toString()
             btnUpdatePassword.setOnClickListener {
+                if (username.isBlank()) {
+                    SweetToast.info(
+                        this@ForgotPasswordActivity,
+                        "Please enter username"
+                    )
+                    return@setOnClickListener
+                } else if (oldPassword == newPassword) {
+                    SweetToast.info(
+                        this@ForgotPasswordActivity,
+                        "New password can not be same as old"
+                    )
+                    return@setOnClickListener
+                } else if (newPassword == confirmPassword) {
+                    SweetToast.info(
+                        this@ForgotPasswordActivity,
+                        "New Password and confirm password does not matched "
+                    )
+                    return@setOnClickListener
+                }
                 doPasswordChange()
             }
         }
